7cd97d0ae65095f4bee554b62e1ff640c08b2c8b
NixonMSilva <nixonmoreira@hotmail.com>
2016-05-18 17:20:40 -0300

    Página de Login
    Implementação do login e cadastro dos dados do formulário no banco de
    dados.

---
+++ _method/create_user.php
@@ -0,0 +1,80 @@
+<?php
+
+/* Autor: Nixon Silva
+ * Data: 18/05/2016
+ * Função: Código acionado quando um usuário clica no botão cadastro, o código
+ * é responsável por efetuar as validações dos dados e o cadastra efetivamente
+ * logo em seguida caso sejam informações válidas.
+ */
+
+/*  TODO: [Nixon Silva]
+ * - Implementar função que impede o cadastro quando um e-mail já está 
+ * registrado no banco de dados.
+ * 
+*/
+
+include 'mysql_connect.php';
+
+// Verifica se os valores digitados são válidos
+if (!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['password_1'])
+        and !empty($_POST['password_2']) and !empty($_POST['sex']) and !empty($_POST['date']))
+{  
+    // Atribui os valores entrado no formulário à variáveis
+    $input_name     = $_POST['name'];
+    $input_email    = $_POST['email'];
+    $input_pass     = $_POST['password_1'];
+    $input_pass_2   = $_POST['password_2'];
+    $input_sex      = $_POST['sex'];
+    $input_date     = $_POST['date'];
+    
+    // Verifica se as senhas são iguais
+    if ($input_pass != $input_pass_2)
+    {
+        echo "<script> 
+            alert('As senhas inseridas são diferentes!');
+            window.location.href='../index.html';
+        </script>";
+        exit;
+    }
+}
+else
+{
+    echo "<script> 
+        alert('Os campos obrigatórios devem ser preenchidos!');
+        window.location.href='../index.html';
+    </script>";
+    exit;
+}
+
+// Proteção contra MySQL Injection
+$input_name     = stripslashes ($input_name);
+$input_email    = stripslashes ($input_email);
+$input_pass     = stripslashes ($input_pass);
+$input_pass_2   = stripslashes ($input_pass_2);
+$input_sex      = stripslashes ($input_sex);
+$input_date     = stripslashes ($input_date);
+$input_name     = mysql_real_escape_string ($input_name);
+$input_email    = mysql_real_escape_string ($input_email);
+$input_pass     = mysql_real_escape_string ($input_pass);
+$input_pass_2   = mysql_real_escape_string ($input_pass_2);
+$input_sex      = mysql_real_escape_string ($input_sex);
+$input_date     = mysql_real_escape_string ($input_date);
+
+// Inserção no banco de dados
+$result = mysqli_query($conn, "INSERT INTO `users`(`username`, `email`, `password`, `sex`, `birthday`) VALUES ('".$input_name."','".$input_email."','".$input_pass."', '".$input_sex."', '".$input_date."')");
+if (!$result)
+{
+    echo "Erro ao inserir no banco de dados!";
+    exit;
+}
+else
+{
+    // Inicia a sessão do usuário
+    session_start ();
+    echo "Cadastro efetuado com sucesso!";   
+}
+
+// Termina a conexão com o banco de dados
+mysqli_close ($conn);
+
+?>
