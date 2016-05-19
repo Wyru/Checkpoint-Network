40d960897dfdce4c5e65c8f557597c30bd0e271d
Will Saymon <wsaymonoficial@gmail.com>
2016-05-19 15:25:52 -0300

    Criação das páginas de perfil e edição de perfil
    Falta colocar o css na página de edição de perfil, mas para isso é
    necessário ter os campos de edição. O mesmo para a página de perfil,
    para continuar é necessário ter o conteúdo da base de dados.

---
+++ my_profile.php
@@ -0,0 +1,123 @@
+<!--Página Meu Perfil-->
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="_css/my_profile.css">
+	<meta charset="utf-8">
+	<title>Meu Perfil</title>
+</head>
+<body>
+	<header>
+		<div id="profilePic">
+			<img src="http://tedxnashville.com/wp-content/uploads/2015/11/profile.png"/>
+			<p>Nome</p>
+		</div>
+		
+		<div id= "navBar">
+			<form class = "search" action="#" method="POST">
+			 <input type="" name="" placeholder=" Pesquisar">
+			</form>
+			<ul>
+				<li> 
+					<div>
+						<a href="#">Página Inicial</a>
+					</div>
+				</li>
+				<li> 
+					<div>
+						<a href="#">Meu Perfil</a>
+					</div>
+				</li>
+				<li> 
+					<div>
+						<a href="#">Screenshots</a>
+					</div>
+				</li>
+				<li> 
+					<div>
+						<a href="#">Gameplays</a>
+					</div>
+				</li>
+				<li> 
+					<div>
+						<a href="#">Game List</a>
+					</div>
+				</li>
+				<li> 
+					<div>
+						<a href="#">Guildas</a>
+					</div>
+				</li>
+				<li> 
+					<div>
+						<a href="#">Amigos</a>
+					</div>
+				</li>
+			</ul>			
+		</div>
+	</header>
+		<section id = "profileBody">
+		<div id="left">
+			<section id="aboutMe">
+				<p><a href="edit_profile.php">Editar Perfil</a></p>
+				<div >
+					<h3>Bio: </h3>
+					<div id="Bio">
+						
+					</div>
+				</div>
+				<div>
+					<h3>Game Preferido:</h3>
+					<div id="favoGame">
+						
+					</div>
+				</div>
+				<div>
+					<h3>Plataforma Preferida:</h3>
+					<div id="favoPlat">
+						
+					</div>
+				</div>
+				<div>
+					<h3>Steam ID: </h3>
+					<div id="steamId">
+						
+					</div>
+				</div>
+				<div>
+					<h3>PSN ID: </h3>
+					<div id="psnId">
+						
+					</div>
+				</div>
+				<div>
+					<h3>Xbox Live ID: </h3>
+					<div id="xboxLive">
+						
+					</div>
+				</div>
+				<div>
+					<h3>Nintendo Network ID: </h3>
+					<div id="nintendoNetworkID">
+						
+					</div>
+				</div>
+			</section>
+		</div>
+		<section id="content">
+			<div id = "timeLine">
+				
+
+			</div>
+			
+		</section>
+
+	</section>
+
+
+</body>
+</html>
