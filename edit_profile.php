91088a4458ba98de94bee1a17862c1a421196c9b
Will Saymon <wsaymonoficial@gmail.com>
2016-06-01 14:58:33 -0300

    Edit profile update

--- edit_profile.php
+++ edit_profile.php
@@ -7,7 +7,7 @@
 /* TODO: Escrever rotina de redefinição de senha
  * 
  */
-
+/*
 // Inicia sessão
 session_start ();
 // Verifica se a variável de login está ativada
@@ -17,12 +17,20 @@ if (!$_SESSION["login_status"])
     header ("location:login.html");
     exit;
 }
+ */
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="_bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="_css/edit_profile.css">
+        
+        <!--Não adicione nada antes disso-->
+
         <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
         <script src="//code.jquery.com/jquery-1.10.2.js"></script>
         <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
@@ -35,94 +43,53 @@ if (!$_SESSION["login_status"])
         <title>Editar Perfil</title>
     </head>
     <body>
-        <header>
-            <div id= "navBar">
-                <ul>
-                    <li> 
-                        <div>
-                            <a href="#">Página Inicial</a>
-                            <head>
-                                <link rel="stylesheet" type="text/css" href="_css/edit_profile.css">
-                                <meta charset="utf-8">
-                                <title>Editar Perfil</title>
-                            </head>
-                            <body>
-                                <header>
-                                    <div id="profilePic">
-                                        <img src="http://tedxnashville.com/wp-content/uploads/2015/11/profile.png"/>
-                                        <p><?php echo $_SESSION["name"]; ?></p>
-                                    </div>
 
-                                    <div id= "navBar">
-                                        <form class = "search" action="search_profile.php" method="POST">
-                                            <input type="text" name="value" placeholder=" Pesquisar">
-                                        </form>
-                                        <ul>
-                                            <li> 
-                                                <div>
-                                                    <a href="home.php"><img src="_imagens/_icones/home.png">Página Inicial</a
-                                                </div>
-                                            </li>
-                                            <li> 
-                                                <div>
-                                                    <a href="#">Meu Perfil</a>
-                                                    <a href="my_profile.php"><img src="_imagens/_icones/profile.png">Meu Perfil</a>
-                                                </div>
-                                            </li>
-                                            <li> 
-                                                <div>
-                                                    <a href="#">Screenshots</a>
-                                                    <a href="my_photos.php"><img src="_imagens/_icones/screenshots.png">Screenshots</a>
-                                                </div>
-                                            </li>
-                                            <li> 
-                                                <div>
-                                                    <a href="#">Gameplays</a>
-                                                    <a href="my_videos"><img src="_imagens/_icones/gamePlay.png">Gameplays</a>
-                                                </div>
-                                            </li>
-                                            <li> 
-                                                <div>
-                                                    <a href="#">Game List</a>
-                                                    <a href="my_games"><img src="_imagens/_icones/games.png">Game List</a>
-                                                </div>
-                                            </li>
-                                            <li> 
-                                                <div>
-                                                    <a href="#">Guildas</a>
-                                                    <a href="my_groups"><img src="_imagens/_icones/guilds.png">Guildas</a>
-                                                </div>
-                                            </li>
-                                            <li> 
-                                                <div>
-                                                    <a href="#">Amigos</a>
-                                                    <a href="my_friends"><img src="_imagens/_icones/friends.png">Amigos</a>
-                                                </div>
-                                            </li>
-                                        </ul>			
-                                    </div>
-                                </header>
-                                <section>
-                                    <div id = "profile">
-                                        <h1>Perfil</h1>
-                                        <form method="POST" action="./_method/edit_user.php">
-                                            <p>Nome:<input type="text" name="new_name" value="<?php echo $_SESSION["name"] ?>"></p>
-                                            <p>Sexo:<select name="sex">
-                                                    <option value="male">Masculino</option>
-                                                    <option value="female">Feminino</option>
-                                                    <option value="other">Outro</option>
-                                                </select></p>
-                                            <p>Data de Nascimento: <input type="text" id="datepicker" name="new_date" value="<?php echo $_SESSION["birthday"] ?>"></p>
-                                            <p>PSN:<input type="text" name="new_psn" value="<?php echo $_SESSION["psn"] ?>"></p>
-                                            <p>Steam:<input type="text" name="new_steam" value="<?php echo $_SESSION["steam"] ?>"></p>
-                                            <p>Live:<input type="text" name="new_live" value="<?php echo $_SESSION["xbox_live"] ?>"></p>
-                                            <p>Nintendo ID:<input type="text" name="new_nintendo" value="<?php echo $_SESSION["nintendo"] ?>"></p>
-                                            <p>Biografia:<textarea name="new_biography"><?php echo $_SESSION["biography"] ?></textarea></p>
-                                            <p>Jogo Favorito:<input type="text" name="new_game" value="<?php echo $_SESSION["favorite_game"] ?>"></p><br>
-                                            <p>Senha Atual:<input type="password" name = "curr_password"/></p>
-                                            <p><button type="submit">Efetivar Alterações</button></p>
-                                        </form>
-                                    </div>
-                        </section>
-                        </body>
-                        </html>
+        <header>
+            <?php include("default_header.php");?>
+        </header>
+        <section class="container" id="content">
+            <div class="row" >
+                <div class="col-lg-12" id="title">
+                    <h1><i class="fa fa-pencil fa-lg"></i>Editar Perfil</h1>
+                </div>
+
+            </div>
+            <div class="row">
+                <form role="form" method="POST" action="./_method/edit_user.php">
+                <div class="col-lg-6 form-group">
+                    <label for="Name">Nome:</label>
+                    <input class="form-control" id="Name" type="text" name="new_name" value="<?php echo $_SESSION["name"] ?>">
+                    <label for="sex">Sexo:</label>
+                    
+                    <select class="form-control" name="sex" id="sex">
+                            <option value="male">Masculino</option>
+                            <option value="female">Feminino</option>
+                            <option value="other">Outro</option>
+                        </select>
+                    <label for="datepicker">Data de Nascimento:</label> 
+                    <input class="form-control" type="text" id="datepicker" name="new_date" value="<?php echo $_SESSION["birthday"] ?>"></p>
+                 
+                </div>
+                <div class="col-lg-6">
+                    <p>PSN:<input type="text" name="new_psn" value="<?php echo $_SESSION["psn"] ?>"></p>
+                    <p>Steam:<input type="text" name="new_steam" value="<?php echo $_SESSION["steam"] ?>"></p>
+                    <p>Live:<input type="text" name="new_live" value="<?php echo $_SESSION["xbox_live"] ?>"></p>
+                    <p>Nintendo ID:<input type="text" name="new_nintendo" value="<?php echo $_SESSION["nintendo"] ?>"></p>
+                    <p>Biografia:<textarea name="new_biography"><?php echo $_SESSION["biography"] ?></textarea></p>
+                    <p>Jogo Favorito:<input type="text" name="new_game" value="<?php echo $_SESSION["favorite_game"] ?>"></p><br>    
+                </div>
+                    <div class="col-lg-12">
+                        <p><button type="submit">Efetivar Alterações</button></p>
+                    </div>
+                </form>
+                
+
+                
+                    
+                    
+                    
+                
+            </div>
+        </section>
+    </body>
+</html>
