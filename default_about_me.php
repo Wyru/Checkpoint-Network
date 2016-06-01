344cdceccf3ba37519e8c6cc06e031319861275d
Will Saymon <wsaymonoficial@gmail.com>
2016-06-01 17:55:17 -0300

    Criação about_me profile
    :v:

---
+++ default_about_me.php
@@ -0,0 +1,85 @@
+
+
+<!--Página Meu Perfil-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="_bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="_css/default_about_me.css">
+        <!--Não adicione nada antes disso-->
+
+    </head>
+    <body>
+            
+        <section class="col-md-12" id="aboutMe">
+                        <div class="row" >
+                            <div class="col-md-12">
+                                <h3>Bio: </h3>
+                                <div id="data">
+                                    <?php echo $_SESSION["biography"]; ?>
+                                </div>   
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Game Preferido:</h3>
+                                <div id="data">
+                                    <?php echo $_SESSION["favorite_game"]; ?>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Plataforma Preferida:</h3>
+                                <div id="data">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Steam ID: </h3>
+                                <div id="data">
+                                    <?php echo $_SESSION["steam"]; ?>
+                                </div>
+                            </div >
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>PSN ID: </h3>
+                                <div id="data">
+                                    <?php echo $_SESSION["psn"]; ?>
+                                </div>
+                            </div >
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Xbox Live ID: </h3>
+                                <div id="data">
+                                    <?php echo $_SESSION["xbox_live"]; ?>
+                                </div>
+                            </div >
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12"> 
+                                <h3>Nintendo Network ID: </h3>
+                                <div id="data">
+                                    <?php echo $_SESSION["nintendo"]; ?>
+                                </div>
+                            </div >
+                        </div >
+                    </div>
+                </section>
+
+            
+        <!--Não coloque  nada abaixo disso-->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>  
+    </body>
+</html>
